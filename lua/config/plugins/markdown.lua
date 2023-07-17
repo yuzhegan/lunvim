333df694729f8d96ab69811d2de2f00fd5d1e528
yuzhegan <boflyyang@163.com>
2023-07-17 13:51:51 +0800

    wsl mdpreview

--- lua/config/plugins/markdown.lua
+++ lua/config/plugins/markdown.lua
@@ -1,13 +1,47 @@
+-- return {
+-- 	{
+-- 		"iamcco/markdown-preview.nvim",
+-- 		ft = { "markdown", "vimwiki" },
+-- 		build = "cd app && yarn install",
+-- 		config = function()
+-- 			vim.g.mkdp_auto_start = 0
+-- 			vim.g.mkdp_filetypes = { 'markdown', 'vimwiki' }
+-- 			vim.g.mkdp_theme = 'dark'
+-- 		end,
+-- 	},
+--
+-- }
+vim.cmd(
+	[[
+function OpenMarkdownPreview (url)
+		execute "silent ! edge --new-window " . a:url
+endfunction
+]]
+)
+
 return {
 	{
 		"iamcco/markdown-preview.nvim",
 		ft = { "markdown", "vimwiki" },
 		build = "cd app && yarn install",
+
 		config = function()
 			vim.g.mkdp_auto_start = 0
-			vim.g.mkdp_filetypes = { 'markdown', 'vimwiki' }
+			vim.g.mkdp_auto_close = 1
 			vim.g.mkdp_theme = 'dark'
-		end,
-	},
+			vim.g.mkdp_open_to_the_world = 1
+			vim.g.mkdp_open_ip = '127.0.0.1'
+			vim.g.mkdp_port = 65141
+			-- vim.g.mkdp_echo_preview_url = 1
+			-- function OpenMarkdownPreview(url)
+			-- 	vim.cmd('silent ! edge --new-window ' .. url)
+			-- 	echo(url)
+			-- 	-- silent ! edge --new-window http://127.0.0.1:65141/page/1
+			-- end
 
+			vim.g.mkdp_filetypes = { 'markdown', 'vimwiki' }
+			vim.g.mkdp_browserfunc = 'OpenMarkdownPreview'
+		end,
+
+	},
 }
