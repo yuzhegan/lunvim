7a5a238cbbd0472a3b12bb6b5bb0b48691c8ae8b
yuzhegan <boflyyang@163.com>
2023-07-22 23:20:24 +0800

    python lsp

--- lua/config/lsp/python.lua
+++ lua/config/lsp/python.lua
@@ -1,27 +1,27 @@
-return {
-	setup = function(lspconfig, lsp)
-		lspconfig.pyright.setup {
-			handlers = {
-				-- pyright ignores dynamicRegistration settings
-				['client/registerCapability'] = function(_, _, _, _)
-					return {
-						result = nil,
-						error = nil,
-					}
-				end
-			},
-			settings = {
-				python = {
-					analysis = {
-						extraPath = { '.' },
-						autoImportCompletions = true,
-					}
-				}
-			}
-
-		}
-	end
-}
+-- return {
+-- 	setup = function(lspconfig, lsp)
+-- 		lspconfig.pyright.setup {
+-- 			handlers = {
+-- 				-- pyright ignores dynamicRegistration settings
+-- 				['client/registerCapability'] = function(_, _, _, _)
+-- 					return {
+-- 						result = nil,
+-- 						error = nil,
+-- 					}
+-- 				end
+-- 			},
+-- 			settings = {
+-- 				python = {
+-- 					analysis = {
+-- 						extraPath = { '.' },
+-- 						autoImportCompletions = true,
+-- 					}
+-- 				}
+-- 			}
+--
+-- 		}
+-- 	end
+-- }
 --
 --
 --
@@ -45,3 +45,47 @@ return {
 -- 	end
 --
 -- }
+
+
+return {
+	setup = function(lspconfig, lsp)
+		lspconfig.pyright.setup {
+			capabilities = lsp.capabilities,
+			flags = { debounce_text_changes = 150 },
+			on_attach = lsp.on_attach,
+			cmd = { "pyright-langserver", "--stdio" },
+			filetypes = { "python" },
+			handlers = {
+				-- pyright ignores dynamicRegistration settings
+				['client/registerCapability'] = function(_, _, _, _)
+					return {
+						result = nil,
+						error = nil,
+					}
+				end
+			},
+			settings = {
+				python = {
+					analysis = {
+						autoSearchPaths = true,
+						diagnosticMode = "workspace",
+						useLibraryCodeForTypes = true,
+						typeCheckingMode = "off",
+					},
+					formatting = {
+						provider = "autopep8",
+						autopep8Args = "--max-line-length=200"
+					},
+					linting = {
+						flake8Enabled = true,
+						flake8Args = "--max-line-length=200"
+					},
+					sortImports = {
+						args = { "--line-length=200", "--multi-line=4" }
+					}
+				},
+			}
+
+		}
+	end
+}
