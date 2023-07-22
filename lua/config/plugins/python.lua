9f5c9e1d3586608f7d8ca37a8fb44cfe6942d7e2
yuzhegan <boflyyang@163.com>
2023-07-22 22:49:40 +0800

    fix E90 fold error

--- lua/config/plugins/python.lua
+++ lua/config/plugins/python.lua
@@ -1,21 +1,17 @@
 vim.cmd([[ func SetPythonTitle()
-  "call setline(1,"# -*- coding: utf-8 -*-")
   call setline(1,"# encoding='utf-8")
   call append(line("."), "")
   call append(line(".")+1, "\# @Time: ".strftime("%Y-%m-%d",localtime()))
   call append(line(".")+2, "\# @File: ".("%"))
 	call append(line(".")+3, "#!/usr/bin/env")
-	"call append(line(".")+4, "\# @Description: ")
 	call append(line(".")+4, "from icecream import ic")
-	"call append(line(".")+5, "import sys")
 	call append(line(".")+5, "import os")
-	" call append(line(".")+6, "os.chdir(os.path.abspath(os.path.dirname(__file__)))")
-	call append(line(".")+7, "#change cwd to current file dir")
  endfunc
 ]])
 
 vim.cmd([[ autocmd BufNewFile *py exec ":call SetPythonTitle()"]])
-vim.cmd([[ autocmd BufNewFile * normal G o]])
+-- let curesor move to the end of the file
+vim.cmd([[ autocmd BufNewFile *py normal G]])
 
 
 return {
